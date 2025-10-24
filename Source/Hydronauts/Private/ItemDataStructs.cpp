b1c1039f22c4533474bd9a93df2f2f6372b52488
Devin Brown <P2798179@LEC-ADMIN.dmu.ac.uk>
2025-10-24 13:01:26 +0100

    Edit: Added Structs

---
+++ Source/Hydronauts/Private/ItemDataStructs.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ItemDataStructs.h"
+
+// Add default functionality here for any IItemDataStructs functions that are not pure virtual.
---
+++ Source/Hydronauts/Public/ItemDataStructs.h
@@ -0,0 +1,100 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "ItemDataStructs.generated.h"
+
+UENUM()
+enum class EItemQuality : uint8
+{
+
+};
+
+UENUM()
+enum class EItemType : uint8
+{
+Collectable UMETA(DisplayName = "Collectable"),
+Quest UMETA(DisplayName = "Quest"),
+};
+
+USTRUCT()
+struct FItemStatistics
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere);
+	float ArmorRating;
+	UPROPERTY(EditAnywhere);
+	float DamageValue;
+};
+
+USTRUCT()
+struct FItemTextData
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere);
+	FText Name;
+
+	UPROPERTY(EditAnywhere);
+	FText Description;
+
+	UPROPERTY(EditAnywhere);
+	FText InteractionText;
+
+	UPROPERTY(EditAnywhere);
+	FText UsageText;
+};
+
+USTRUCT()
+struct FItemNumericData
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere);
+	float Weight;
+
+	UPROPERTY(EditAnywhere);
+	int32 MaxStackSize;
+
+	UPROPERTY(EditAnywhere);
+	bool bIsStackable;
+};
+
+USTRUCT()
+struct FItemAssetData
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere);
+	UTexture2D* Icon;
+
+	UPROPERTY(EditAnywhere);
+	UStaticMesh* Mesh;
+};
+
+USTRUCT()
+struct FItemData : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, Category = "Item Data");
+	EItemType ItemType;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data");
+	EItemQuality ItemQuality;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data");
+	FItemStatistics ItemStatistics;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data");
+	FItemTextData TextData;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data");
+	FItemNumericData NumericData;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data");
+	FItemAssetData AssetData;
+
+};
+
