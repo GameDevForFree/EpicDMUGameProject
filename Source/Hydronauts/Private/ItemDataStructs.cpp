8c86b6991bbd5608c26da7c45877dbceedd7b12f
MockDeath09 <dfobbester2020@gmail.com>
2025-10-30 16:52:45 +0000

    Edit: Commented All My Changes

--- Source/Hydronauts/Private/ItemDataStructs.cpp
+++ Source/Hydronauts/Private/ItemDataStructs.cpp
@@ -1,6 +1,6 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "ItemDataStructs.h"
-
-// Add default functionality here for any IItemDataStructs functions that are not pure virtual.
+//// Fill out your copyright notice in the Description page of Project Settings.
+//
+//
+//#include "ItemDataStructs.h"
+//
+//// Add default functionality here for any IItemDataStructs functions that are not pure virtual.
