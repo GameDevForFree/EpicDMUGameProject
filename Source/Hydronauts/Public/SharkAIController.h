8d307b5a659e3223ab63d367f1f5448951032758
Hornet <149481390+Hornet-VR@users.noreply.github.com>
2025-11-04 18:00:41 +0000

    new line added because i forgot to save

--- Source/Hydronauts/Public/SharkAIController.h
+++ Source/Hydronauts/Public/SharkAIController.h
@@ -17,5 +17,6 @@ class HYDRONAUTS_API ASharkAIController : public AAIController
 protected:
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaSeconds) override;
+	virtual void LineOfSightTo(AActor);
 	APawn* PlayerPawn;
 };
