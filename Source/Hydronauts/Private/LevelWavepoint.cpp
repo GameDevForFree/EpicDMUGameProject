8df6e8c8d040b307aa65bd345760d000535e5dcf
Cory Young <GameDevForFree@gmail.com>
2025-11-04 12:12:40 +0000

    Added->LevelWaypoint cpp + blueprint
    Not a fully working LevelWaypoint, but all the things required are there.
    
    Working on something else in the meantime so I'm gonna put this on hold and come back later

---
+++ Source/Hydronauts/Private/LevelWavepoint.cpp
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LevelWavepoint.h"
+//#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+ALevelWavepoint::ALevelWavepoint()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//creating the box triggers
+	Volume = CreateDefaultSubobject<UBoxComponent>(TEXT("Volume"));
+	Volume->InitBoxExtent(FVector(100.f, 400.f, 500.f));
+	Volume->SetCollisionResponseToAllChannels(ECR_Overlap);
+	Volume->SetupAttachment(GetRootComponent());
+
+}
+
+// Called when the game starts or when spawned
+void ALevelWavepoint::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALevelWavepoint::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ALevelWavepoint::PostInitializeComponents() 
+{
+	Super::PostInitializeComponents();
+
+	Volume->OnComponentBeginOverlap.AddDynamic(this, &ALevelWavepoint::OnVolumeBeginOverlap);
+	Volume->OnComponentEndOverlap.AddDynamic(this, &ALevelWavepoint::OnVolumeEndOverlap);
+}
+
+void ALevelWavepoint::OnVolumeBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (Cast<APlayerController>(OtherActor))
+	{
+		
+		GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Yellow, FString::Printf(TEXT("added rotation")));
+		////adds the level to a list
+		//GetWorld()->AddLevel(NextLevel);
+		////removed the current level from the list, forcing the player into the next one
+		//GetWorld()->RemoveLevel(GetWorld()->GetCurrentLevel());
+	}
+	GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Yellow, FString::Printf(TEXT("detected collision")));
+}
+
+void ALevelWavepoint::OnVolumeEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	//triggers on collision exit
+}
+
---
+++ Source/Hydronauts/Public/LevelWavepoint.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
+#include "LevelWavepoint.generated.h"
+
+UCLASS()
+class HYDRONAUTS_API ALevelWavepoint : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ALevelWavepoint();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	void PostInitializeComponents();
+
+	UFUNCTION()
+	void OnVolumeBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+
+	UFUNCTION()
+	void OnVolumeEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UPROPERTY(EditAnywhere, Category = "Teleporter")
+	UBoxComponent* Volume;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Teleporter")
+	UWorld* NextLevel;
+};
